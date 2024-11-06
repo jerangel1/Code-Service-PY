08d6393e78e497db3e7064dbed796c3ffd314618
jerangel1 <jerangel1691@gmail.com>
2024-11-06 12:16:01 -0400

    [Update] production

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from sqlalchemy.orm import Session
 from src.config.database import get_db
 from src.models.email_account import EmailAccount
 from src.services.email_service import EmailCodeService
+import os
+
 
 app = FastAPI()
 
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["streaming-tv-ydpz.vercel.app"],
+    allow_origins=os.getenv("ALLOWED_ORIGINS"),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
